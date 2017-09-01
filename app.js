965269cf6aa5bf290d7bc8aadf5101906d26db34
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-01 15:47:45 -0700

    add config

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 const app = express();
+const keys = require("./config/keys");
+
 
 mongoose.Promise = global.Promise;
 
 // Use testing database for testing
 if (process.env.NODE_ENV !== "test")
-  mongoose.connect("mongodb://localhost/music", { useMongoClient: true });
+  mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 // middleware
 app.use(bodyParser.json());
---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:admin1975@ds127730.mlab.com:27730/react-feedback"
+};
