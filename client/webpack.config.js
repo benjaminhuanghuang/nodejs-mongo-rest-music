ed17d5fa563c0ffa0b4404a9d1d2d5f0c223f41f
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-02 20:45:53 -0700

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,14 +21,6 @@ module.exports = {
       {
         test: /\.css$/,
         loader: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./[hash].[ext]"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
       }
     ]
   },
